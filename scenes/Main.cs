025a96d65991e04d5e3dcb8087df0e9df2f0e14f
Aria Edmonds <aria@ar1as.space>
2024-09-27 19:22:35 +0800

    Begin

---
+++ scenes/Main.cs
@@ -0,0 +1,15 @@
+using System;
+using Godot;
+
+public partial class Main : Node2D
+{
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        GD.Print("Hello World!");
+        GD.Print("a");
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta) { }
+}
