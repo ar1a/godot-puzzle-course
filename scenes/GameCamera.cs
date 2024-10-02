48b6dcc4747b44dc5dc7c0cc101763a5927806ac
Aria Edmonds <aria@ar1as.space>
2024-10-02 14:29:30 +0800

    add mouse panning the camera

--- scenes/GameCamera.cs
+++ scenes/GameCamera.cs
@@ -8,12 +8,27 @@ public partial class GameCamera : Camera2D
     private readonly StringName ACTION_PAN_RIGHT = "pan_right";
     private readonly StringName ACTION_PAN_UP = "pan_up";
     private readonly StringName ACTION_PAN_DOWN = "pan_down";
+    private readonly StringName ACTION_MOVE_MAP = "move_map";
     private readonly int PAN_SPEED = 500;
     private readonly int TILE_SIZE = 64;
+
+    private Vector2 fixedTogglePoint = Vector2.Zero;
 
     public override void _Process(double delta)
     {
         GlobalPosition = GetScreenCenterPosition();
+
+        if (Input.IsActionJustPressed("move_map"))
+        {
+            fixedTogglePoint = GetViewport().GetMousePosition();
+        }
+        if (Input.IsActionPressed("move_map"))
+        {
+            var mousePosition = GetViewport().GetMousePosition();
+            GlobalPosition -= (mousePosition - fixedTogglePoint);
+            fixedTogglePoint = mousePosition;
+        }
+
         var moveDir = Input.GetVector(
             ACTION_PAN_LEFT,
             ACTION_PAN_RIGHT,
